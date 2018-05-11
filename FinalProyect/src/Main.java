64fcfcc9e8abf2c2574fd24ee97b69da56036448
Cristobal <crpina@alumnos.uai.cl>
2018-05-11 17:04:00 -0300

    java fx added

--- FinalProyect/src/Main.java
+++ FinalProyect/src/Main.java
@@ -1,51 +1,53 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import java.util.Scanner;
 
 
-public class Main {
+public class Main extends Application {
 	private Assistant assistant;
 	public Main() {
 		this.assistant = new Assistant();
 	}
-	public void salute() {
-		System.out.println("Hello Human.");
-		System.out.println("I'm a phone simulator, I already have installed your assistant.");
-		System.out.println("And it's ready to help you.");
-	}
 	
-	public void displayOptions() {
-		System.out.println("");
-		System.out.println("What would you like me to do?");
-		System.out.println("");
-		System.out.println("1.- Simulate incoming call.");
-		System.out.println("2.- Simulate incoming news.");
-		System.out.println("3.- Turn off.\n");
-	}
-	
-	public void exit() {
-		System.out.println("Good Bye...");
-		System.exit(0);
-	}
-	
-	public void executeOptionSelected() {
-		Scanner reader = new Scanner(System.in);
-		System.out.println("Select an option: ");
-		int n = reader.nextInt();
-		reader.close();
-		switch(n) {
-			case 1:
-				this.assistant.getCall();
-				break;
-			case 2:
-				this.assistant.getNews();
-			case 3:
-				this.exit();
-		}
-	}
 	public static void main(String[] args) {
-		Main phone = new Main();
-		phone.salute();
-		phone.displayOptions();
-		phone.executeOptionSelected();
+		// Main phone = new Main();
+		launch(args);
 
 	}
+	public HBox addFooter() {
+		HBox footer = new HBox();
+        footer.setPadding(new Insets(25, 12, 25, 12));
+        footer.setSpacing(10);
+        footer.setStyle("-fx-background-color: #dcdcdc;" + 
+        "-fx-border-color: #000000;" + "-fx-border-width: 2;");
+        
+        Button buttonCurrent = new Button("Toggle");
+        buttonCurrent.setPrefSize(100, 20);
+        Circle circle = new Circle(10, 10, 10);
+        circle.setFill(javafx.scene.paint.Color.RED);
+        footer.getChildren().addAll(buttonCurrent, circle);
+        footer.setAlignment(Pos.CENTER_RIGHT);
+        return footer;
+	}
+	@Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Mobile Assistant");
+        
+        BorderPane border = new BorderPane();
+       
+        border.setBottom(addFooter());
+        
+        primaryStage.setScene(new Scene(border, 600, 400));
+        primaryStage.show();
+    }
 }
