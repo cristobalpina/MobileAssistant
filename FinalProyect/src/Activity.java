86e807022732ea8dc500a539bbdc855874f2eb8d
Cristobal <crpina@alumnos.uai.cl>
2018-06-22 02:31:19 -0400

    final delivery
    final delivery

---
+++ FinalProyect/src/Activity.java
@@ -0,0 +1,11 @@
+
+public class Activity {
+	private String name;
+	public Activity(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
--- FinalProyect/src/Assistant.java
+++ FinalProyect/src/Assistant.java
@@ -6,6 +6,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -17,11 +19,13 @@ public class Assistant extends Application {
 	private boolean busy;
 	private LinkedList<Call> missedCalls;
 	private LinkedList<News> missedNews;
+	private LinkedList<Activity> activities;
 
 	public Assistant() {
 		this.busy = false;
 		this.missedCalls = new LinkedList<Call>();
 		this.missedNews = new LinkedList<News>();
+		this.activities = new LinkedList<Activity>();
 	}
 
 	public void getCall() {
@@ -46,6 +50,10 @@ public class Assistant extends Application {
 		return this.busy;
 	}
 
+	public void removeActivity() {
+		this.activities.remove(0);
+	}
+
 	public void setBusy(boolean value) {
 		if (this.busy) {
 			this.clearPendingCalls();
@@ -53,6 +61,15 @@ public class Assistant extends Application {
 		}
 		this.busy = value;
 
+	}
+
+	public LinkedList<Activity> getActivities() {
+		return this.activities;
+	}
+
+	public void addActivity(String name) {
+		Activity activity = new Activity(name);
+		this.activities.add(activity);
 	}
 
 	public LinkedList<Text> getNotifications() {
@@ -66,6 +83,7 @@ public class Assistant extends Application {
 			String title = this.missedNews.get(i).getTitle();
 			notifications.add(new Text("Missed News: " + title));
 		}
+		notifications.add(new Text("Next Activity: " + this.activities.pop().getName()));
 		System.out.println(notifications);
 		return notifications;
 	}
@@ -107,11 +125,19 @@ public class Assistant extends Application {
 		BorderPane border = new BorderPane();
 		HBox footer = addFooter();
 		VBox body = addBody();
+		VBox leftBar = addBody();
+		Label label1 = new Label("Activity:");
+		TextField textField = new TextField();
 		Circle circle = new Circle(10, 10, 10);
 		circle.setFill(javafx.scene.paint.Color.GREEN);
-
+		HBox input = new HBox();
+		input.getChildren().addAll(label1, textField);
+		input.setSpacing(10);
 		Button toggleBtn = new Button("Not Busy");
 		toggleBtn.setPrefSize(100, 20);
+		Button addActivityBtn = new Button("Add Activity");
+		Button removeActivityBtn = new Button("Remove Activity");
+
 		toggleBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
@@ -123,6 +149,12 @@ public class Assistant extends Application {
 					System.out.println(notifications);
 					body.getChildren().clear();
 					body.getChildren().addAll(notifications);
+					leftBar.getChildren().clear();
+					for (Activity activity : assistant.getActivities()) {
+						leftBar.getChildren().add(0, new Text(activity.getName()));
+					}
+					
+					leftBar.getChildren().addAll(input, addActivityBtn, removeActivityBtn);
 				} else {
 					toggleBtn.setText("Busy");
 					circle.setFill(javafx.scene.paint.Color.RED);
@@ -165,8 +197,38 @@ public class Assistant extends Application {
 		footer.getChildren().addAll(newsBtn, callBtn, toggleBtn, circle);
 		footer.setAlignment(Pos.CENTER_RIGHT);
 
+	
+		
+		addActivityBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				String text = textField.getText();
+				if (text.length() > 0) {
+					assistant.addActivity(text);
+					textField.clear();
+					leftBar.getChildren().add(0, new Text(text));
+				}
+
+			}
+		});
+
+		removeActivityBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				assistant.removeActivity();
+				leftBar.getChildren().clear();
+				for (Activity activity : assistant.getActivities()) {
+					leftBar.getChildren().add(0, new Text(activity.getName()));
+				}
+				leftBar.getChildren().addAll(input, addActivityBtn, removeActivityBtn);
+			}
+		});
+
+		leftBar.getChildren().addAll(input, addActivityBtn, removeActivityBtn);
+		leftBar.setAlignment(Pos.TOP_CENTER);
 		border.setBottom(footer);
 		border.setCenter(body);
+		border.setLeft(leftBar);
 		primaryStage.setScene(new Scene(border, 600, 400));
 		primaryStage.show();
 	}
